82cfb0d1bf292b99f03156eb50253f7da03533a4
liwentao0705 <liwentao0705@outlook.com>
2016-03-15 20:09:03 +0800

    vendor: add dlmalloc

--- config/he_common.mk
+++ config/he_common.mk
@@ -25,3 +25,17 @@ ifeq ($(DISABLE_ADB_AUTH),true)
         ro.adb.secure=3 \
         persist.sys.root_access=3
 endif
+
+# Lower RAM devices
+ifeq ($(LOW_RAM_DEVICE),true)
+MALLOC_IMPL := dlmalloc
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_atlas=true \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+endif
